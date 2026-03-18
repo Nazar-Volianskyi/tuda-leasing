4ef62a9fa3247de9696e7d1e142588794030a843
Nazar-Volianskyi <volyanskiy.n@gmail.com>
2026-03-18 14:56:13 +0100

    add first entities, Volianskyi

---
+++ src/main/java/com/bobocode/tudaleasing/entity/CarSpec.java
@@ -0,0 +1,64 @@
+package com.bobocode.tudaleasing.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "car_specs")
+@Setter
+@Getter
+@NoArgsConstructor
+public class CarSpec {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "car_id")
+    private Car car;
+
+    @Column(name = "body_type", length = 50)
+    private String bodyType;
+
+    @Column(name = "doors")
+    private Long doors;
+
+    @Column(name = "seats")
+    private Long seats;
+
+    @Column(name = "engine_capacity", precision = 4, scale = 2)
+    private BigDecimal engineCapacity;
+
+    @Column(name = "engine_power")
+    private Long enginePower;
+
+    @Column(name = "fuel_type", length = 50)
+    private String fuelType;
+
+    @Column(length = 50)
+    private String gearbox;
+
+    @Column(name = "drive_type", length = 50)
+    private String driveType;
+
+    @Column(name = "max_speed")
+    private Integer maxSpeed;
+
+    @Column(name = "acceleration_0_100", precision = 4, scale = 1)
+    private BigDecimal acceleration0100;
+
+    @Column(name = "fuel_tank_capacity")
+    private Long fuelTankCapacity;
+
+    @Column(name = "trunk_capacity")
+    private Long trunkCapacity;
+
+    @Column(name = "weight")
+    private Long weight;
+
+}
---
+++ src/main/java/com/bobocode/tudaleasing/entity/Color.java
@@ -0,0 +1,32 @@
+package com.bobocode.tudaleasing.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "colors")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Color {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String name;
+
+    @Column(name = "hex_code", length = 10)
+    private String hexCode;
+
+    @OneToMany(mappedBy = "color")
+    @Setter(AccessLevel.PRIVATE)
+    private List<Car> categories = new ArrayList<>();
+}

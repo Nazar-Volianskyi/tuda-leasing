09912865928de809950dc048dae20db6d0362c71
Nazar-Volianskyi <volyanskiy.n@gmail.com>
2026-03-18 14:57:28 +0100

    add first entities, Volianskyi

---
+++ src/main/java/com/bobocode/tudaleasing/entity/Car.java
@@ -0,0 +1,58 @@
+package com.bobocode.tudaleasing.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "cars")
+@Getter @Setter
+@NoArgsConstructor
+public class Car {
+
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "model_id")
+    private Model model;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "color_id")
+    private Color color;
+
+    @Column(name = "year")
+    private Long year;
+
+    @Column(name = "full_price", precision = 10, scale = 2)
+    private BigDecimal fullPrice;
+
+    @Column(columnDefinition = "text")
+    private String description;
+
+    @Column(name = "description")
+    private Boolean available = true;
+
+    @Column(name = "created_at", insertable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @OneToMany(mappedBy = "car", cascade = CascadeType.ALL)
+    private List<CarImage> images = new ArrayList<>();
+
+    @OneToOne(mappedBy = "car", cascade = CascadeType.ALL)
+    private CarSpec specs;
+}
